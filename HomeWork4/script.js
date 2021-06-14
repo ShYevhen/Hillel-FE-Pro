e3e6eff52520d1c9026b38835188b69eda5d1c38
ShYevhen <shvorak.yevhen@gmail.com>
2021-06-14 20:23:13 +0300

    HW4 fix

--- HomeWork4/script.js
+++ HomeWork4/script.js
@@ -1,6 +1,8 @@
-function obtainUserInput(message) {
+function obtainUserInput(message, validInput) {
     let userInput = prompt(message);
-    return userInput;
+    if (validateUserInput(userInput, validInput)) {
+        return Number.parseFloat(userInput);
+    }
 }
 
 function validateUserInput(userInput, validInput) {
@@ -14,9 +16,9 @@ const calculateArea = (radius) => radius ** 2 * Math.PI;
 const calculateCircumference = (radius) => 2 * Math.PI * radius;
 
 function obtainOperation(enterOperationMsg, validOperations) {
-    let selectedOperation = obtainUserInput(enterOperationMsg);
-    if (validateUserInput(selectedOperation, validOperations)) {
-        return selectedOperation;
+    let selectedOperation = obtainUserInput(enterOperationMsg, validOperations);
+    if (selectedOperation) {
+        return Number.parseInt(selectedOperation);
     } else {
         alert("Нет такой команды");
         return obtainOperation(enterOperationMsg, validOperations);
@@ -25,28 +27,28 @@ function obtainOperation(enterOperationMsg, validOperations) {
 
 const enterRadiusMsg = "Введите радиус круга:";
 const enterOperationMsg = "Введите номер команды чтобы посчитать: 1 - диаметр, 2 - площадь круга, 3 - длинну окружности";
-const validRadius = /^[1-9]{1}[0-9]{0,}$/;
+const validRadius = /^([1-9]{1}[0-9]{0,})|([0-9]{0,}.[0-9]{0,}){0,1}$/;
 const validOperations = /^[1-3]{1}$/;
-const availableOperations = {
-    1: {
+const availableOperations = [
+    {
         message: "Диаметр окружности с радиусом {radius} равен {result}",
         calculate: calculateDiameter,
     },
-    2: {
+    {
         message: "Площадь круга с радиусом {radius} равена {result}",
         calculate: calculateArea,
     },
-    3: {
+    {
         message: "Длинна окружности с радиусом {radius} равена {result}",
         calculate: calculateCircumference,
-    }
-};
+    },
+];
 
-let radius = obtainUserInput(enterRadiusMsg);
-if (validateUserInput(radius, validRadius)) {
+let radius = obtainUserInput(enterRadiusMsg, validRadius);
+if (radius) {
     let operation = obtainOperation(enterOperationMsg, validOperations);
-    let result = availableOperations[operation].calculate(+radius);
-    alert(availableOperations[operation].message.replace("{radius}", radius).replace("{result}", result));
+    let result = availableOperations[operation - 1].calculate(+radius);
+    alert(availableOperations[operation - 1].message.replace("{radius}", radius).replace("{result}", result));
 } else {
     alert("Нужно ввести число");
 }
